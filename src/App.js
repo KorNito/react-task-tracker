79358fff3b2c650663c6dbd50ec93ccea311a938
KorNito <kornelijuspacevicius@gmail.com>
2021-02-04 04:43:42 +0200

    added delete request

--- src/App.js
+++ src/App.js
@@ -28,7 +28,11 @@ function App() {
     setTasks([...task, newTask])
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
